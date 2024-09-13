81ca48995a1172a7bc4c6d06d8d18004a5ea1282
Alena Vinogradova <sol.vinogradova2017@yandex.ru>
2024-09-13 14:18:34 +0300

    Complete 3 tasks

---
+++ README.md
@@ -0,0 +1,1 @@
+# aston_vinogradova_java
---
+++ Task1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число (a): ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введите второе число (b): ");
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = 0;
+
+        if (b != 0) {
+            quotient = (double) a / b;
+        } else {
+            System.out.println("Деление на ноль невозможно");
+        }
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        if (b != 0) {
+            System.out.println("Частное: " + quotient);
+        }
+
+        scanner.close();
+    }
+}
